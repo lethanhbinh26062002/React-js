be347954dc35babd8cdd45b66cedf3ba671bb62d
Lê Thanh Bình <lethanhbinh26062002@gmail.com>
2022-03-11 10:56:55 +0700

    B3: tutor onchange onclick

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,38 @@
-import { useState } from 'react'
+import React,{ useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// const ShowInfo = (props) => {
+//   return <div> ShowInfo {props.name}</div>
+// }
 
+function App() {
+  const [count, setCount] = useState(0) ;
+  const [color, setColor] = useState("blue");
+  const [myStatus,setMyStatus] = useState(false);
+  const [products, setProducts] = useState([{id:1,name: 'A'},{id:2,name: 'B'}]);
+  const changeCount = () => {
+    setCount(1);
+  }
+  const changeStatus = () => {
+    setMyStatus(!myStatus);
+  }
+  const changeColor = () => {
+    setColor("green");
+  }
+  const removeItem = (id) => {
+    const newProduct = products.filter(item => item.id !== id);
+    setProducts(newProduct);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      Count: {count} <br/>
+      <button onClick={changeCount}>ChangeCount</button>
+      Colors: <div style={{background: color,width:100,height:100}}>Color</div> <br/>
+      <button onClick={changeColor}>Change Color</button>
+      Status : {myStatus ? "True" : "False"} <br/>
+      <button onClick={changeStatus}>Change Status</button>
+      {myStatus && <div>Products: {products.map(item =><div>{item.name}<button onClick={() => removeItem(item.id)}>Delete</button></div> )}</div>}
     </div>
   )
 }
