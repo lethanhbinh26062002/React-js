04975b8e3de9bf10896180c663675ccdff5f6050
Lê Thanh Bình <77135879+lethanhbinh26062002@users.noreply.github.com>
2024-09-30 16:00:37 +0700

    revert code

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import { useState } from 'react';
 import { Navigate, NavLink, Route, Routes } from 'react-router-dom';
 import './App.css';
+import ShowInfo from './components/ShowInfo';
+import { Product } from './types/product';
 import HomePage from './pages/HomePage';
 import ProductPage from './pages/ProductPage';
 import AboutPage from './pages/AboutPage';
@@ -7,8 +10,18 @@ import AdminLayout from './pages/Layouts/AdminLayout';
 import Dashboard from './pages/Layouts/Dashboard';
 import WebsiteLayout from './pages/Layouts/WebsiteLayout';
 function App() {
+  // const [count,setCount] = useState<number>(0)
+  // const [info,setInfo] = useState<Product>({
+  //   age: 19,
+  //   name: "Binh"
+  // })
+
   return (
     <div className="App">
+      {/* {count} <button onClick={() => setCount(count +1)}>setCount</button><br/>
+      <ShowInfo person={info} />
+      <h3>{info.name} </h3>
+      <h4>{info.age}</h4> */}
       <header>
         <nav>
           <ul>
@@ -28,7 +41,7 @@ function App() {
             <Route path="/about" element={<AboutPage />} />
           </Route>
           <Route path="/admin" element={<AdminLayout />}>
-            {/* Truy cập /admin thì index chuyển đến /admin/dashboard thông qua Navigate */}
+            {/* Truy cập admin thì index chuyển đến admin/dashboard thông qua Navigate */}
             <Route index element={<Navigate to="/admin/dashboard" />} />
             <Route path="dashboard" element={<Dashboard />} />
           </Route>
